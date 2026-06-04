b1b59070af22848abe85d78bf43abf21e29000e3
Fany Montay <145678763+FMontay@users.noreply.github.com>
2026-06-04 12:12:57 +0300

    Remove unnecessary comment about DISCORD_TOKEN
    Removed commented reminder about changing DISCORD_TOKEN before pushing.

--- main.py
+++ main.py
@@ -20,9 +20,6 @@ load_dotenv()
 
 initialize()
 
-
-# FAAANYYYYYYYYYYY CHANGE EN DISCORD_TOKEN_PROD AVANT DE PUSH SUR GITHUBBBBBBBBB HAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-#token = os.getenv('DISCORD_TOKEN_DEV')
 token = os.getenv('DISCORD_TOKEN')
 
 #Create log folder if it doesn't already exist and set up log file name
